a1296278b507c0f3cb2ba98a95701db68df33f34
David Logie <davidlogie@mac.com>
2010-03-18 12:00:40 +0000

    The 'unknown' value can now be set via the '--unknown' option.

--- tests.py
+++ tests.py
@@ -13,7 +13,7 @@ class Base(unittest.TestCase):
         return location
 
     def unknown(self):
-        process = subprocess.Popen('./bin/vcprompt --unknown'.split(),
+        process = subprocess.Popen('./bin/vcprompt --values unknown'.split(),
                                 stdout=subprocess.PIPE)
         output = process.communicate()[0].strip()
         return output
